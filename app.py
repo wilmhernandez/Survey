e30f098213544ba0031e3737eeb815208e7abcad
wilmhernandez <hernandez.wilbert.m@gmail.com>
2023-08-27 21:20:47 -0700

    Created base and home html markup

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "Let's GO"
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.route('/')
+def home():
+    """Directs user to Homepage"""
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template('home.html', title=title, instructions=instructions)
