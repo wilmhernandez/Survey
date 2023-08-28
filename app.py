60acec0ccb1767f3c3efa3f28248e223339b37e3
wilmhernandez <hernandez.wilbert.m@gmail.com>
2023-08-27 22:28:16 -0700

    Completed questions.html markup

--- app.py
+++ app.py
@@ -17,3 +17,16 @@ def home():
     title = satisfaction_survey.title
     instructions = satisfaction_survey.instructions
     return render_template('home.html', title=title, instructions=instructions)
+
+@app.route('/questions/<ques>')
+def question(ques):
+    """Directs user to questions of the questionnaire"""
+    total_ques = len(satisfaction_survey.questions)
+    display_num = int(ques) + 1
+    ques = int(ques)
+    q3_list = satisfaction_survey.questions[2].choices
+    if int(ques) >= total_ques:
+        return f"Please send request with and interger less than {total_ques} "
+    else:
+        question = satisfaction_survey.questions[ques].question
+    return render_template('questions.html', display=display_num, question=question, ques=ques, list=q3_list)
