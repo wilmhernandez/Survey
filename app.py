e7fdeaab62d2021e9da24e9584de1f72cb6e471e
wilmhernandez <hernandez.wilbert.m@gmail.com>
2023-08-30 21:18:57 -0700

    Completed step 7

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "Let's GO"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -14,6 +15,7 @@ responses = []
 @app.route('/')
 def home():
     """Directs user to Homepage"""
+    responses.clear()
     title = satisfaction_survey.title
     instructions = satisfaction_survey.instructions
     return render_template('home.html', title=title, instructions=instructions)
@@ -30,3 +32,40 @@ def question(ques):
     else:
         question = satisfaction_survey.questions[ques].question
     return render_template('questions.html', display=display_num, question=question, ques=ques, list=q3_list)
+
+
+
+# @app.route(f'/qustions/<ques>')
+# def questions(ques):
+#     if (responses == None):
+#         return redirect("/")
+    
+#     if (len(responses) == len(satisfaction_survey.questions)):
+#         return redirect("/thankyou")
+    
+#     if (len(responses) != ques):
+#         flash('Please answer all questions in order')
+#         redirect(f'/qustions/{len(responses)}')
+        
+#     question = satisfaction_survey.questions[ques].question
+#     display_num = int(ques) + 1
+#     q3_list = satisfaction_survey.questions[2].choices
+#     return render_template('questions.html', display=display_num, question=question, ques=ques, list=q3_list)
+    
+
+@app.route("/answer", methods=["POST"])
+def add_answer():
+    """Adds user answer to responses list"""
+    answer = request.form["option"]
+    responses.append(answer)
+    next_question_num = len(responses)
+    if next_question_num < len(satisfaction_survey.questions):
+        return redirect(f"/questions/{next_question_num}")
+    else:
+        return redirect("/thankyou")
+    
+    
+@app.route("/thankyou")
+def thank_you():
+    """Thanks the user after completing the survey"""
+    return render_template('thank_you.html', responses=responses)
